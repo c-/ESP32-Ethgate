fc6dc41891498b2eb7203dd52c9efb04ec27811a
Christophe Beauregard <beauregardcp@gmail.com>
2024-04-17 21:13:44 -0400

    first stab at recreating the firmware

---
+++ ESP32-Ethgate-firmware/Private.h
@@ -0,0 +1,10 @@
+#ifndef PRIVATE_H
+#define PRIVATE_H
+
+#define WIFI_CHANNEL    7
+#define MQTT_SERVER "192.168.1.24"
+#define MQTT_USER "esp"
+#define MQTT_PASS "especially"
+#define OTA_PASSWORD "will work 4 food"
+
+#endif
